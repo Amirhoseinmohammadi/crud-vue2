6f2650e14e27e11936ee42a66e5c010a7d42aaa8
Amirhoseinmohammadi <amirmi951@gmail.com>
2023-08-21 17:27:13 +0330

    firstcommit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <div>
+      <b-navbar type="dark" variant="dark">
+        <b-navbar-nav>
+          <b-nav-item>
+            <router-link class="btn btn-info" to="/">users</router-link>
+          </b-nav-item>
+          <b-nav-item>
+            <router-link class="btn btn-warning" to="/about"
+              >Go to about</router-link
+            >
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-navbar>
+    </div>
+
+    <div><router-view></router-view></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+}
+</script>
+
+<style></style>
---
+++ src/components/AboutPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    style="height: 70"
+    class="d-flex justify-content-center align-items-center w-50 h-50"
+  >
+    <h1 class="">This is about page</h1>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
